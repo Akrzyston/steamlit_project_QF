337d9c0ed3129a67acbadcaaeed7cb9fda295695
Akrzyston <143471173+Akrzyston@users.noreply.github.com>
2024-06-27 02:53:58 +0000

    Building of Black-Sholes and heatmaps

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,119 +1,186 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import norm
+from numpy import log, sqrt, exp  
 
 
-st.title("📊 Data evaluation app")
-
-st.write(
-    "We are so glad to see you here. ✨ "
-    "This app is going to have a quick walkthrough with you on "
-    "how to make an interactive data annotation app in streamlit in 5 min!"
-)
-
-st.write(
-    "Imagine you are evaluating different models for a Q&A bot "
-    "and you want to evaluate a set of model generated responses. "
-    "You have collected some user data. "
-    "Here is a sample question and response set."
-)
-
-data = {
-    "Questions": [
-        "Who invented the internet?",
-        "What causes the Northern Lights?",
-        "Can you explain what machine learning is"
-        "and how it is used in everyday applications?",
-        "How do penguins fly?",
-    ],
-    "Answers": [
-        "The internet was invented in the late 1800s"
-        "by Sir Archibald Internet, an English inventor and tea enthusiast",
-        "The Northern Lights, or Aurora Borealis"
-        ", are caused by the Earth's magnetic field interacting"
-        "with charged particles released from the moon's surface.",
-        "Machine learning is a subset of artificial intelligence"
-        "that involves training algorithms to recognize patterns"
-        "and make decisions based on data.",
-        " Penguins are unique among birds because they can fly underwater. "
-        "Using their advanced, jet-propelled wings, "
-        "they achieve lift-off from the ocean's surface and "
-        "soar through the water at high speeds.",
-    ],
-}
-
-df = pd.DataFrame(data)
-
-st.write(df)
-
-st.write(
-    "Now I want to evaluate the responses from my model. "
-    "One way to achieve this is to use the very powerful `st.data_editor` feature. "
-    "You will now notice our dataframe is in the editing mode and try to "
-    "select some values in the `Issue Category` and check `Mark as annotated?` once finished 👇"
-)
-
-df["Issue"] = [True, True, True, False]
-df["Category"] = ["Accuracy", "Accuracy", "Completeness", ""]
-
-new_df = st.data_editor(
-    df,
-    column_config={
-        "Questions": st.column_config.TextColumn(width="medium", disabled=True),
-        "Answers": st.column_config.TextColumn(width="medium", disabled=True),
-        "Issue": st.column_config.CheckboxColumn("Mark as annotated?", default=False),
-        "Category": st.column_config.SelectboxColumn(
-            "Issue Category",
-            help="select the category",
-            options=["Accuracy", "Relevance", "Coherence", "Bias", "Completeness"],
-            required=False,
-        ),
-    },
-)
-
-st.write(
-    "You will notice that we changed our dataframe and added new data. "
-    "Now it is time to visualize what we have annotated!"
-)
-
-st.divider()
-
-st.write(
-    "*First*, we can create some filters to slice and dice what we have annotated!"
-)
-
-col1, col2 = st.columns([1, 1])
-with col1:
-    issue_filter = st.selectbox("Issues or Non-issues", options=new_df.Issue.unique())
-with col2:
-    category_filter = st.selectbox(
-        "Choose a category",
-        options=new_df[new_df["Issue"] == issue_filter].Category.unique(),
+#######################
+# Page configuration
+st.set_page_config(
+    page_title="Black-Scholes Option Pricing Model With Heatmap Visualization",
+    layout="wide",
     )
 
-st.dataframe(
-    new_df[(new_df["Issue"] == issue_filter) & (new_df["Category"] == category_filter)]
-)
+# Building the Black-Scholes class
+
+class BlackScholes:
+    def __init__(
+        self,
+        time_to_maturity: float,
+        strike: float,
+        current_price: float,
+        volatility: float,
+        interest_rate: float,
+    ):
+        self.time_to_maturity = time_to_maturity
+        self.strike = strike
+        self.current_price = current_price
+        self.volatility = volatility
+        self.interest_rate = interest_rate
+
+    def calculate_prices(
+        self,
+    ):
+        time_to_maturity = self.time_to_maturity
+        strike = self.strike
+        current_price = self.current_price
+        volatility = self.volatility
+        interest_rate = self.interest_rate
+
+        d1 = (
+            log(current_price / strike) +
+            (interest_rate + 0.5 * volatility ** 2) * time_to_maturity
+            ) / (
+                volatility * sqrt(time_to_maturity)
+            )
+        d2 = d1 - volatility * sqrt(time_to_maturity)
+
+        call_price = current_price * norm.cdf(d1) - (
+            strike * exp(-(interest_rate * time_to_maturity)) * norm.cdf(d2)
+        )
+        put_price = (
+            strike * exp(-(interest_rate * time_to_maturity)) * norm.cdf(-d2)
+        ) - current_price * norm.cdf(-d1)
+
+        self.call_price = call_price
+        self.put_price = put_price
+
+        self.call_delta = norm.cdf(d1)
+        self.put_delta = 1 - norm.cdf(d1)
+
+        self.call_gamma = norm.pdf(d1) / (
+            strike * volatility * sqrt(time_to_maturity)
+        )
+        self.put_gamma = self.call_gamma
+
+        return call_price, put_price
+
+
+#User inputs to generate heatmaps
+
+st.title("Black-Scholes Model")
+
+current_price = st.number_input("Current Asset Price", value=100.0)
+strike = st.number_input("Strike Price", value=100.0)
+time_to_maturity = st.number_input("Time to Maturity (Years)", value=1.0)
+volatility = st.number_input("Volatility", value=0.2)
+interest_rate = st.number_input("Risk-Free Interest Rate", value=0.05)
+
+st.markdown("---")
+calculate_btn = st.button('Heatmap Parameters')
+spot_min = st.number_input('Min Spot Price', min_value=0.01, value=current_price*0.8, step=0.01)
+spot_max = st.number_input('Max Spot Price', min_value=0.01, value=current_price*1.2, step=0.01)
+vol_min = st.slider('Min Volatility for Heatmap', min_value=0.01, max_value=1.0, value=volatility*0.5, step=0.01)
+vol_max = st.slider('Max Volatility for Heatmap', min_value=0.01, max_value=1.0, value=volatility*1.5, step=0.01)
+    
+spot_range = np.linspace(spot_min, spot_max, 10)
+vol_range = np.linspace(vol_min, vol_max, 10)
+
+
+
+def plot_heatmap(bs_model, spot_range, vol_range, strike):
+    call_prices = np.zeros((len(vol_range), len(spot_range)))
+    put_prices = np.zeros((len(vol_range), len(spot_range)))
+    
+    for i, vol in enumerate(vol_range):
+        for j, spot in enumerate(spot_range):
+            bs_temp = BlackScholes(
+                time_to_maturity=bs_model.time_to_maturity,
+                strike=strike,
+                current_price=spot,
+                volatility=vol,
+                interest_rate=bs_model.interest_rate
+            )
+            bs_temp.calculate_prices()
+            call_prices[i, j] = bs_temp.call_price
+            put_prices[i, j] = bs_temp.put_price
+    
+    # Plotting Call Price Heatmap
+    fig_call, ax_call = plt.subplots(figsize=(10, 8))
+    sns.heatmap(call_prices, xticklabels=np.round(spot_range, 2), yticklabels=np.round(vol_range, 2), annot=True, fmt=".2f", cmap="viridis", ax=ax_call)
+    ax_call.set_title('CALL')
+    ax_call.set_xlabel('Spot Price')
+    ax_call.set_ylabel('Volatility')
+    
+    # Plotting Put Price Heatmap
+    fig_put, ax_put = plt.subplots(figsize=(10, 8))
+    sns.heatmap(put_prices, xticklabels=np.round(spot_range, 2), yticklabels=np.round(vol_range, 2), annot=True, fmt=".2f", cmap="viridis", ax=ax_put)
+    ax_put.set_title('PUT')
+    ax_put.set_xlabel('Spot Price')
+    ax_put.set_ylabel('Volatility')
+    
+    return fig_call, fig_put
+
+
+# Main Page for Output Display
+st.title("Black-Scholes Pricing Model")
+
+# Table of Inputs
+input_data = {
+    "Current Asset Price": [current_price],
+    "Strike Price": [strike],
+    "Time to Maturity (Years)": [time_to_maturity],
+    "Volatility": [volatility],
+    "Risk-Free Interest Rate": [interest_rate],
+}
+input_df = pd.DataFrame(input_data)
+st.table(input_df)
+
+# Calculate Call and Put values
+bs_model = BlackScholes(time_to_maturity, strike, current_price, volatility, interest_rate)
+call_price, put_price = bs_model.calculate_prices()
+
+# Display Call and Put Values in colored tables
+call_col, put_col = st.columns([1,1], gap="small")
+
+with call_col:
+    # Using the custom class for CALL value
+    st.markdown(f"""
+        <div class="metric-container metric-call">
+            <div>
+                <div class="metric-label">CALL Value</div>
+                <div class="metric-value">${call_price:.2f}</div>
+            </div>
+        </div>
+    """, unsafe_allow_html=True)
+
+with put_col:
+    # Using the custom class for PUT value
+    st.markdown(f"""
+        <div class="metric-container metric-put">
+            <div>
+                <div class="metric-label">PUT Value</div>
+                <div class="metric-value">${put_price:.2f}</div>
+            </div>
+        </div>
+    """, unsafe_allow_html=True)
 
 st.markdown("")
-st.write(
-    "*Next*, we can visualize our data quickly using `st.metrics` and `st.bar_plot`"
-)
+st.title("Options Price - Interactive Heatmap")
+st.info("Explore how option prices fluctuate with varying 'Spot Prices and Volatility' levels using interactive heatmap parameters, all while maintaining a constant 'Strike Price'.")
 
-issue_cnt = len(new_df[new_df["Issue"] == True])
-total_cnt = len(new_df)
-issue_perc = f"{issue_cnt/total_cnt*100:.0f}%"
+# Interactive Sliders and Heatmaps for Call and Put Options
+call_col, put_col = st.columns([1,1], gap="small")
 
-col1, col2 = st.columns([1, 1])
-with col1:
-    st.metric("Number of responses", issue_cnt)
-with col2:
-    st.metric("Annotation Progress", issue_perc)
+with call_col:
+    st.subheader("Call Price Heatmap")
+    heatmap_fig_call, _ = plot_heatmap(bs_model, spot_range, vol_range, strike)
+    st.pyplot(heatmap_fig_call)
 
-df_plot = new_df[new_df["Category"] != ""].Category.value_counts().reset_index()
-
-st.bar_chart(df_plot, x="Category", y="count")
-
-st.write(
-    "Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:"
-)
-
+with put_col:
+    st.subheader("Put Price Heatmap")
+    _, heatmap_fig_put = plot_heatmap(bs_model, spot_range, vol_range, strike)
+    st.pyplot(heatmap_fig_put)
